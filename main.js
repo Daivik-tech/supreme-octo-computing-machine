cdb561c3c3faead8be1bc38ddf21a49cddbc322e
Daivik-tech <84041224+Daivik-tech@users.noreply.github.com>
2021-12-11 20:18:29 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img=''
+
+function preload()
+{
+img=loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+canvas=createCanvas(640,420);
+canvas.center();
+}
+
+function draw()
+{
+image(img,0,0,0,640,420);
+fill('#FF000');
+text('Dog',45,75);
+nofill();
+stroke('#FF000');
+rect(30,60,450,350);    
+}
